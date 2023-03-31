83e7865b4b520023539dfd49f17fa0fd02729bd3
altinhysa <100517587+altinhysa@users.noreply.github.com>
2023-03-31 17:32:34 +0200

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,16 +2,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author TiniGonaXheni
- */
 public class ShoppingCart {
     private List<Item> items;
     
